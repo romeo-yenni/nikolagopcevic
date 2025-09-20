30b5809267885bd1efdd454f3eab1ca78694740d
nik <nikolag000@hotmail.com>
2025-09-20 09:22:56 +0800

    remove

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -90,17 +90,6 @@ const Works = () => {
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
-      <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          The following projects showcase my skills and experience through real
-          world examples of my works. Mainly utilizing React for frontends and
-          Django for backends. Also some projects use Tailwind CSS.
-        </motion.p>
-      </div>
-
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
